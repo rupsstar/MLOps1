4cdafb0ff0e987f6d15f55504732bcac9f40e63f
Raghvendra Lavekar <rupsstar@gmail.com>
2024-08-01 21:03:49 +0530

    added files

--- model.py
+++ model.py
@@ -8,8 +8,7 @@ iris = load_iris()
 X, y = iris.data, iris.target
 
 # Split dataset
-X_train, X_test, y_train, y_test = train_test_split(
-X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
 model = RandomForestClassifier()
