3ce184931cc9d4492d237d44f68a7b4b13fd50d9
Raghvendra Lavekar <rupsstar@gmail.com>
2024-08-02 08:21:53 +0530

    changed the model file

--- model.py
+++ model.py
@@ -3,17 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load dataset
+# Load dataset test
 iris = load_iris()
 X, y = iris.data, iris.target
 
 # Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
-<<<<<<< HEAD
 X, y, test_size=0.2, random_state=42)
-=======
-  X, y, test_size=0.2, random_state=42)
->>>>>>> 01f790578fbbc45ce20f7a86fe45b88d8958ea7a
 
 # Train model
 model = RandomForestClassifier()
@@ -22,8 +18,4 @@ model.fit(X_train, y_train)
 # Predict and evaluate
 predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
-<<<<<<< HEAD
 print(f"Model Accuracy: {accuracy:.2f}")
-=======
-print(f"Model Accuracy: {accuracy:.2f}")
->>>>>>> 01f790578fbbc45ce20f7a86fe45b88d8958ea7a
